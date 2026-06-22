0a506e68c968427a9cc0963b077d9b95d44e1172
ekaa8415 <ekaa84150@gmail.com>
2026-06-22 20:30:19 +0700

    Fix Bug Register + Login

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 from routers import transactions # Import file router transaksi
 from auth import router as auth_router # 1. IMPORT ROUTER AUTH YANG BARU LU BIKIN!
 
+from database import engine
+import models
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 # Daftarkan router transaksi ke aplikasi utama FastAPI
